4875401054e958ead5b18a05409c72c1e6cb31ef
David Négrier <d.negrier@thecodingmachine.com>
2020-03-14 22:59:23 +0100

    Adding continuous integration

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -23,4 +23,27 @@ function expectError() {
     echo "Successfully tested: $1"
 }
 
+# First parameter: Test name
+# Second parameter: libsonnet test file
+# Third parameter: JSON Path (as interpreted by jq)
+# Fourth parameter: Value expected
+function expectValue() {
+    echo "Running test: $1"
+
+    OUTPUT=`jsonnet "$2" | jq "$3"`
+    if [[ $? != 0 ]]; then
+        echo "Jsonnet returned an error code"
+        exit 1
+    fi
+    (echo $OUTPUT | grep "$3") > /dev/null
+    if [[ $OUTPUT != "$4" ]]; then
+        echo "Expected '$4', got '$OUTPUT'"
+        exit 1
+    fi
+
+    echo "Successfully tested: $1"
+}
+
+
 expectError "Testing creation of host without a port" "host_without_port.libsonnet" "Can't create container by deployment without any port with deeployer"
+expectValue "Testing creation of ingress when a host is added" "host.libsonnet" "Can't create container by deployment without any port with deeployer"
