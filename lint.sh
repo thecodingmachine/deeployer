62a5058120c89258a089e5a1718c60852366d6ad
Mika Diallo <m.diallo@thecodingmachine.com>
2020-04-09 15:51:52 +0200

    Added Json-format-cli commands to linting

--- lint.sh
+++ lint.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 
+# Formatting (j/lib)sonnet files
 cd lib/deeployer
 jsonnetfmt -i *.*sonnet
 cd ../../tests
 jsonnetfmt -i *.*sonnet
 cd docker-compose-prod
 jsonnetfmt -i *.*sonnet
+
+# Getting back to root
+cd ../..
+
+# Formatting json files
+json-format *.json
+cd environments/default/
+json-format spec.json
+cd ../../tests/schema
+json-format *.json
+
+
+
