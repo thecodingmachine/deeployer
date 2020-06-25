e715272fe7b9e0e99cb8ba7c36c854d8ba4a0d1f
Mika Diallo <m.diallo@thecodingmachine.com>
2020-06-25 18:30:00 +0200

    Extand phpUnitTesting

--- tests/parseConfigForCredentialsTest.php
+++ tests/parseConfigForCredentialsTest.php
@@ -15,7 +15,9 @@ class parseConfigForCredentialsTest extends TestCase
         }
         
         $expected = [
+            "kubectl -n toto delete secret aa827ffc96199a7071140cc2267bc1b1a",
             "kubectl -n toto create secret docker-registry aa827ffc96199a7071140cc2267bc1b1a --docker-server=testUrl.com --docker-username=mika --docker-password='secret' --docker-email=mika\n",
+            "kubectl -n toto delete secret a7ef8300d82557df5ead6d4576d6388cd",
             "kubectl -n toto create secret docker-registry a7ef8300d82557df5ead6d4576d6388cd --docker-server=testUrl2.com --docker-username=mika --docker-password='secret' --docker-email=mika\n",
         ];
         $this->assertEquals($expected, $returnArray);
